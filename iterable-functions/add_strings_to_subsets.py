4f18879d49343acd68ff4c4de30e04034aefe45a
MForofontov <mykyta.forofontov@hotmail.com>
2024-11-11 15:51:07 +0000

    Feat GITHUB-4 Started working on Iterable functions

--- iterable-functions/add_strings_to_subsets.py
+++ iterable-functions/add_strings_to_subsets.py
@@ -17,7 +17,17 @@ def add_strings_to_subsets(my_list: List[Set[str]], my_strings: List[str]) -> bo
     -------
     bool
         True if any string was added to a set, False otherwise.
+
+    Raises
+    ------
+    TypeError
+        If my_list is not a list of sets or my_strings is not a list of strings.
     """
+    if not isinstance(my_list, list) or not all(isinstance(sublist, set) for sublist in my_list):
+        raise TypeError("my_list must be a list of sets")
+    if not isinstance(my_strings, list) or not all(isinstance(s, str) for s in my_strings):
+        raise TypeError("my_strings must be a list of strings")
+
     found = False
     for my_string in my_strings:
         if found:
