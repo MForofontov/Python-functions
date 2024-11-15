6171aacb183ba1319c349f41fc9cd9f3625d5150
MForofontov <mykyta.forofontov@hotmail.com>
2024-11-15 16:23:17 +0000

    GITHUB-4  feat: added new unitests

--- iterable_functions/get_shared_elements.py
+++ iterable_functions/get_shared_elements.py
@@ -14,7 +14,17 @@ def get_shared_elements(dict_: Dict[str, List[Any]]) -> List[Any]:
     -------
     list
         A list containing elements that appear in at least two lists within the dictionary.
+
+    Raises
+    ------
+    TypeError
+        If dict_ is not a dictionary or if any value in dict_ is not a list.
     """
+    if not isinstance(dict_, dict):
+        raise TypeError("dict_ must be a dictionary")
+    if not all(isinstance(value, list) for value in dict_.values()):
+        raise TypeError("All values in dict_ must be lists")
+
     all_elements = [elem for sublist in dict_.values() for elem in sublist]
     element_counts = Counter(all_elements)
     shared_elements = [elem for elem, count in element_counts.items() if count >= 2]
