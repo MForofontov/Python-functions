a0ef2def2bb7a279f013cea8181af60741576959
MForofontov <mykyta.forofontov@hotmail.com>
2024-12-20 17:01:13 +0000

    feat: working on decorators unit tests

--- decorators/enforce_types.py
+++ decorators/enforce_types.py
@@ -1,6 +1,7 @@
-from typing import Callable, Any, get_type_hints
+import logging
+from typing import Callable, Any, get_type_hints, Optional
 
-def enforce_types(func: Callable[..., Any]) -> Callable[..., Any]:
+def enforce_types(func: Callable[..., Any], logger: Optional[logging.Logger] = None) -> Callable[..., Any]:
     """
     A decorator to enforce type hints on the arguments of a function.
 
@@ -8,6 +9,8 @@ def enforce_types(func: Callable[..., Any]) -> Callable[..., Any]:
     ----------
     func : Callable[..., Any]
         The function to be decorated.
+    logger : Optional[logging.Logger]
+        The logger to use for logging errors. If None, the default logger is used.
 
     Returns
     -------
@@ -44,6 +47,21 @@ def enforce_types(func: Callable[..., Any]) -> Callable[..., Any]:
         for arg_name, arg_value in zip(hints.keys(), args):
             expected_type = hints[arg_name]
             if not isinstance(arg_value, expected_type):
-                raise TypeError(f"Expected {expected_type} for argument '{arg_name}', got {type(arg_value)}.")
+                error_message = f"Expected {expected_type} for argument '{arg_name}', got {type(arg_value)}."
+                if logger:
+                    logger.error(error_message, exc_info=True)
+                else:
+                    raise TypeError(error_message)
+        for arg_name, arg_value in kwargs.items():
+            if arg_name in hints:
+                expected_type = hints[arg_name]
+                if not isinstance(arg_value, expected_type):
+                    error_message = f"Expected {expected_type} for argument '{arg_name}', got {type(arg_value)}."
+                    if logger:
+                        logger.error(error_message, exc_info=True)
+                    else:
+                        raise TypeError(error_message)
         return func(*args, **kwargs)
+
     return wrapper
+    
